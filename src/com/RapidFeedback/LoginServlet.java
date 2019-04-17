ea08d5f102b5212451ac8c856373ac2f465d7c7e
Tanzk2018 <49629750+Tanzk2018@users.noreply.github.com>
2019-04-17 14:28:00 +1000

    Update LoginServlet.java

--- src/com/RapidFeedback/LoginServlet.java
+++ src/com/RapidFeedback/LoginServlet.java
@@ -1,11 +1,18 @@
 package com.RapidFeedback;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 
 /**
  * Servlet implementation class LoginServlet
@@ -35,7 +42,31 @@ public class LoginServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		
+		InsideFunction function = new InsideFunction();
+
+		JSONObject jsonReceive;
+		BufferedReader reader = request.getReader();
+		String str, wholeString = "";
+	    while((str = reader.readLine()) != null)
+	    {
+	        wholeString += str;  
+	    }
+	    System.out.println("Receive: " + wholeString);
+	    jsonReceive = JSON.parseObject(wholeString);
+		String username = jsonReceive.getString("username");
+		String password = jsonReceive.getString("password");
+	    int login_ACK;
+		try {
+			 	login_ACK = function.Login(username, password);
+			 	JSONObject jsonSend = new JSONObject();
+			 	jsonSend.put("login_ACK", login_ACK);
+			 	PrintWriter output = response.getWriter();
+			 	output.print(jsonSend.toJSONString());
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+	   
 	}
 
 }
