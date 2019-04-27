573a410bf6e1137847484af718afa1c7d4d62f39
Tanzk2018 <49629750+Tanzk2018@users.noreply.github.com>
2019-04-27 15:10:02 +1000

    delete and import student

--- src/servlets/DeleteStudentServlet.java
+++ src/servlets/DeleteStudentServlet.java
@@ -1,11 +1,19 @@
 package servlets;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.RapidFeedback.MysqlFunction;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 
 /**
  * Servlet implementation class DeleteStudentServlet
@@ -34,8 +42,46 @@ public class DeleteStudentServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		//		
+		MysqlFunction dbFunction = new MysqlFunction();
+		
+		//get JSONObject from request
+		JSONObject jsonReceive;
+		BufferedReader reader = request.getReader();
+		String str, wholeString = "";
+	    while((str = reader.readLine()) != null)
+	    {
+	        wholeString += str;  
+	    }
+	    System.out.println("Receive: " + wholeString);
+	    jsonReceive = JSON.parseObject(wholeString);
+	    
+	    //get values from received JSONObject
+		String token = jsonReceive.getString("token");
+		String projectName = jsonReceive.getString("projectName");
+		String studentID = jsonReceive.getString("studentID");
+		
+		ServletContext servletContext = this.getServletContext();
+				
+		/*
+		 * Attention:
+		 * This method is to delete the student whose studentID is 'studentID':
+		 * we assume the StudentID cannot change and is the primary key.
+		 */
+		
+		boolean updateStudent_ACK;
+	    //Mention:
+		//call the SQL method to edit the student information whose studentID is studentID.
+		//return the 'true' or 'false' value to update_ACK
+		updateStudent_ACK = false;
+		
+		//construct the JSONObject to send
+		JSONObject jsonSend = new JSONObject();
+		jsonSend.put("updateStudent_ACK", updateStudent_ACK);
+		
+		//send
+		PrintWriter output = response.getWriter();
+	 	output.print(jsonSend.toJSONString());
 	}
 
 }
