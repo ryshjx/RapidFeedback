222089a884e73a700e9676dfd186ae28b5aa5ef1
Dinghao <vicydh@gmail.com>
2019-05-09 17:40:49 +1000

    update Mark.java

--- src/com/RapidFeedback/Mark.java
+++ src/com/RapidFeedback/Mark.java
@@ -6,6 +6,19 @@ public class Mark {
 
     private ArrayList<Criteria> criteriaList = new ArrayList<Criteria>();
     private ArrayList<Double> markList = new ArrayList<Double>();
+    private String comment;
+
+    public String getComment() {
+
+        return comment;
+    
+    }
+
+    public void setComment(String comment) {
+
+        this.comment = comment;
+
+    }
 
     public void setCriteriaList(ArrayList<Criteria> criteriaList) {
 
