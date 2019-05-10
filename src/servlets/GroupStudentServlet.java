1ef9e2a887d8e4c38d9916c143ed9427d3074d07
Tanzk2018 <49629750+Tanzk2018@users.noreply.github.com>
2019-05-10 20:26:49 +1000

    Update GroupStudentServlet.java

--- src/servlets/GroupStudentServlet.java
+++ src/servlets/GroupStudentServlet.java
@@ -1,11 +1,19 @@
 package servlets;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.RapidFeedback.MysqlFunction;
+import com.RapidFeedback.StudentInfo;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 
 /**
  * Servlet implementation class GroupStudentServlet
@@ -35,7 +43,44 @@ public class GroupStudentServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+//		
+		MysqlFunction dbFunction = new MysqlFunction();
+		
+		//get JSONObject from request
+		JSONObject jsonReceive;
+		BufferedReader reader = request.getReader();
+		String str, wholeString = "";
+	    while((str = reader.readLine()) != null)
+	    {
+	        wholeString += str;  
+	    }
+	    System.out.println("Receive: " + wholeString);
+	    jsonReceive = JSON.parseObject(wholeString);
+	    
+	    //get values from received JSONObject
+		String token = jsonReceive.getString("token");
+		String projectName = jsonReceive.getString("projectName");
+		String studentID = jsonReceive.getString("studentID");
+		int email = jsonReceive.getInteger("group");
+		
+		ServletContext servletContext = this.getServletContext();
+		StudentInfo student = new StudentInfo(studentID, firstName, middleName, lastName, email);
+				
+		
+		boolean updateStudent_ACK;
+	    //Mention:
+		//call the SQL method to edit the student groupID whose studentID is studentID.
+		//return the 'true' or 'false' value to update_ACK
+		
+	//	updateStudent_ACK = 
+		
+		//construct the JSONObject to send
+		JSONObject jsonSend = new JSONObject();
+		jsonSend.put("updateStudent_ACK", updateStudent_ACK);
+		
+		//send
+		PrintWriter output = response.getWriter();
+	 	output.print(jsonSend.toJSONString());
 	}
 
 }
