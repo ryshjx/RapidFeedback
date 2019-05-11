44fe23cb1892b3adb9b0f42bae4578eaac3e0ab6
Jingxian Hu <jingxianh@student.unimelb.edu.au>
2019-05-11 22:30:06 +1000

    AWS SSH KEY

--- src/servlets/LoginServlet.java
+++ src/servlets/LoginServlet.java
@@ -39,7 +39,8 @@ public class LoginServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.getWriter().append("Test!!!");
 	}
 
 	/**
